87605397e02e914ba1cdaa54d0e6868c126b87b5
Kevin J. Zea <kevinjzea@gmail.com>
2023-09-14 16:29:15 -0600

    New configuration using Nextjs, TypeScript and Tailwind CSS created

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-24">
+      <h1 className="text-3xl">Hello world!</h1>
+    </main>
+  );
+};
+
+export default Home;
