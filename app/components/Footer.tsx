9257482538d30e6da81657bc516dccc0248b47a1
Kevin J. Zea <kevinjzea@gmail.com>
2023-09-18 20:01:36 -0600

    feat: beta footer created

---
+++ app/components/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CustomSocialIcon } from './CustomSocialIcon';
+
+const Footer = () => {
+  return (
+    <footer className="p-6">
+      <div className="flex gap-3 flex-wrap justify-center items-center">
+        <CustomSocialIcon
+          title="LinkedIn"
+          url="https://linkedin.com/in/kevinjzea"
+        />
+        <CustomSocialIcon
+          title="GitHub"
+          url="https://github.com/kevinjzea"
+        />
+        <CustomSocialIcon
+          network="x"
+          title="X"
+          url="https://twitter.com/kevinjzea"
+        />
+        <CustomSocialIcon
+          title="Instagram"
+          url="https://instagram.com/kevinjzea"
+        />
+        <CustomSocialIcon
+          title="YouTube"
+          url="https://youtube.com/@kevinjzea"
+        />
+        <CustomSocialIcon url="https://platzi.com/p/kevinjzea" />
+      </div>
+    </footer>
+  );
+};
+
+export { Footer };
